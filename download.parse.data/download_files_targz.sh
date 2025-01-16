885ecc1a1401161282c6121748d67c02923553b9
Chananchida Sang-aram VSCODE <sai.chananchida@gmail.com>
2025-01-16 16:11:27 +0700

    additional scripts for downloading from PMC

---
+++ download.parse.data/download_files_targz.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# FTP server details
+FTP_SERVER="ftp.ncbi.nlm.nih.gov"
+
+DIRECTORIES=(
+    "comm"
+    "noncomm"
+    "other"
+)
+LOCAL_DIR="article_tarfiles"
+
+# Ensure the local directory exists
+mkdir -p "$LOCAL_DIR"
+
+# Range of PMC IDs to download
+DATE="2024-12-18"
+
+# Loop through directories
+for DIR in "${DIRECTORIES[@]}"; do
+
+	# Loop through PMC IDs from 0 to 11
+	for i in $(seq -w 0 11); do
+	    FILENAME="oa_${DIR}_xml.PMC0${i}xxxxxx.baseline.${DATE}.tar.gz"
+	    FTP_DIR="/pub/pmc/oa_bulk/oa_${DIR}/xml/"
+	    URL="ftp://${FTP_SERVER}${FTP_DIR}${FILENAME}"
+	    LOCAL_FILE="$LOCAL_DIR/$FILENAME"
+
+	    # Skip download if the file already exists
+            if [ -f "$LOCAL_FILE" ]; then
+            	echo "$FILENAME already exists. Skipping download."
+	        continue
+            fi
+
+	    echo "Downloading $FILENAME..."
+	    wget -P "$LOCAL_DIR" "$URL"
+
+	    if [ $? -eq 0 ]; then
+		echo "$FILENAME downloaded successfully."
+	    else
+		echo "Failed to download $FILENAME."
+	    fi
+
+	done
+done
+
+echo "Download complete."
+
